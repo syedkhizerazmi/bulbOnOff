fdc6e5a63d6707586aa5a544c3dde2bfe8304a42
syedkhizerazmi <syedkhizerazmi5@gmail.com>
2018-07-30 18:08:26 +0500

    lol

---
+++ script.js
@@ -0,0 +1,8 @@
+function lightOn(){
+    let light=document.querySelector("#bulb");
+    light.innerHTML= "<img src='pic_bulbon.gif' class='img1'>";
+}
+function lightoff(){
+	let lightof=document.querySelector("#bulb");
+	lightof.innerHTML="<img src='pic_bulboff.gif' class='img1'>";
+}
